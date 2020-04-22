0bd7947ef21f689d8021a45a6f2ba9f83106fb29
nagendraambig <nagendraambig79@gmail.com>
2020-04-22 16:48:25 +0530

    react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { Navbar, NavbarBrand, NavLink, NavItem, Nav } from 'reactstrap';
+import Menu from './components/Menu';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    {/* //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a */}
+    {/* //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a> */}
+    {/* //   </header> */}
+    <Navbar dark color="primary">
+      <div className="container">
+        <NavbarBrand href="/">Restorant  Con Fusion</NavbarBrand>
+        <Nav className="mr-auto" navbar>
+        <NavItem>
+            <NavLink href="/">Home</NavLink>
+        </NavItem>
+        </Nav>
+      </div>
+    </Navbar>
+    <Menu />
     </div>
   );
 }
